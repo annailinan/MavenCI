2dee5b97199d548c60facea01bfe5569ef4a1f31
Anna Li <Kristusha3@yandex.ru>
2023-04-23 10:09:49 +0300

    finish

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -9,7 +9,7 @@ public class StatisticsServiceTest {
 
     @Test
     void findMax() {
-        StatisticsService service = new StatisticsService();
+        ru.netology.statistic.StatisticsService service = new ru.netology.statistic.StatisticsService();
 
         long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
         long expected = 12;
